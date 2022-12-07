3aec68044edbc0e642dea7792449293e057a5ff2
Dimitrios Pliakos <dimitrispl96@gmail.com>
2022-12-07 18:13:28 +0200

    codedump

--- src/train.py
+++ src/train.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import numpy as np
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+# from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
 from sklearn.model_selection import cross_val_predict
